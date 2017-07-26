8d5ff390edd2cd440afcfdc73d484e0b51a8e890
krathjen <kurt.rathjen@gmail.com>
2017-07-26 11:07:41 +0100

    Add support for a "dont show message again" option in the messagebox

--- packages/studioqt/widgets/messagebox.py
+++ packages/studioqt/widgets/messagebox.py
@@ -11,63 +11,240 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-from studioqt import QtGui
+import os
+
 from studioqt import QtCore
 from studioqt import QtWidgets
 
 import studioqt
 
+HOME_PATH = os.getenv('APPDATA') or os.getenv('HOME')
+SETTINGS_PATH = os.path.join(HOME_PATH, 'studioqt.ini')
+
+
+def createMessageBox(
+        parent,
+        title,
+        message,
+        buttons=None,
+        headerIcon=None,
+        headerColor=None,
+        enableDontShowCheckBox=False
+):
+    """
+    Open a question message box with the given options.
+
+    :type parent: QWidget
+    :type title: str
+    :type message: str
+    :type buttons: list[QMessageBox.StandardButton]
+    :type headerIcon: str
+    :type headerColor: str
+    :type enableDontShowCheckBox: bool
+
+    :rtype: MessageBox
+    """
+    mb = MessageBox(parent, enableDontShowCheckBox=enableDontShowCheckBox)
+
+    mb.setText(message)
+
+    buttons = buttons or QtWidgets.QDialogButtonBox.Ok
+    mb.setButtons(buttons)
+
+    if headerIcon:
+        p = studioqt.resource.pixmap(headerIcon)
+        mb.setPixmap(p)
+
+    headerColor = headerColor or "rgb(50, 150, 200)"
+    mb.setHeaderColor(headerColor)
+
+    mb.setWindowTitle(title)
+    mb.setTitleText(title)
+
+    return mb
+
+
+def showMessageBox(
+        parent,
+        title,
+        message,
+        buttons=None,
+        headerIcon=None,
+        headerColor=None,
+        enableDontShowCheckBox=False,
+        force=False
+    ):
+    """
+    Open a question message box with the given options.
+
+    :type parent: QWidget
+    :type title: str
+    :type message: str
+    :type buttons: list[QMessageBox.StandardButton]
+    :type headerIcon: str
+    :type headerColor: str
+    :type enableDontShowCheckBox: bool
+    :type force: bool
+
+    :rtype: MessageBox
+    """
+    settings = QtCore.QSettings(SETTINGS_PATH, QtCore.QSettings.IniFormat)
+
+    key = 'MessageBox/{}/'.format(title.replace(" ", "_"))
+
+    clickedButton = int(settings.value(key + "clickedButton") or -1)
+    dontShowAgain = settings.value(key + "dontShowAgain") == "true"
+
+    # Force show the dialog if the user is holding the ctrl key down
+    if studioqt.isControlModifier() or studioqt.isAltModifier():
+        force = True
+
+    if force or not dontShowAgain or not enableDontShowCheckBox:
+
+        mb = createMessageBox(
+            parent,
+            title,
+            message,
+            buttons=buttons,
+            headerIcon=headerIcon,
+            headerColor=headerColor,
+            enableDontShowCheckBox=enableDontShowCheckBox
+        )
+
+        mb.exec_()
+        mb.close()
+
+        # Save the button that was clicked by the user
+        clickedButton = mb.clickedStandardButton()
+        settings.setValue(key + "clickedButton", clickedButton)
+
+        # Save the dont show again checked state
+        dontShowAgain = mb.isDontShowCheckboxChecked()
+        settings.setValue(key + "dontShowAgain", dontShowAgain)
+
+    return clickedButton
+
 
 class MessageBox(QtWidgets.QDialog):
 
     @staticmethod
-    def question(parent, title, message, options):
+    def question(
+        parent,
+        title,
+        message,
+        buttons=None,
+        headerIcon="question",
+        headerColor="rgb(50, 150, 200)",
+        enableDontShowCheckBox=False
+    ):
+        """
+        Open a question message box with the given options.
 
-        mb = MessageBox(parent)
-        mb.setText(message)
-        mb.setOptions(options)
-        mb.header().setStyleSheet("background-color: rgb(50,150,200);")
+        :type parent: QWidget
+        :type title: str
+        :type message: str
+        :type headerIcon: str
+        :type headerColor: str
+        :type buttons: list[QMessageBox.StandardButton]
 
-        p = studioqt.resource.pixmap("question")
-        mb.setPixmap(p)
+        :rtype: QMessageBox.StandardButton
+        """
+        clickedButton = showMessageBox(
+            parent,
+            title,
+            message,
+            buttons=buttons,
+            headerIcon=headerIcon,
+            headerColor=headerColor,
+            enableDontShowCheckBox=enableDontShowCheckBox,
+        )
 
-        mb.setWindowTitle(title)
-        mb.setTitleText(title)
-
-        a = mb.exec_()
-        mb.close()
-
-        return mb._standardButtonClicked
+        return clickedButton
 
     @staticmethod
-    def critical(parent, title, message):
+    def warning(
+        parent,
+        title,
+        message,
+        buttons=None,
+        headerIcon="warning2",
+        headerColor="rgb(200, 128, 0)",
+        enableDontShowCheckBox=False,
+        force=False,
+    ):
+        """
+        Open a warning message box with the given options.
 
-        mb = MessageBox(parent)
-        mb.setText(message)
-        mb.setOptions(QtWidgets.QDialogButtonBox.Ok)
-        mb.header().setStyleSheet("background-color: rgb(200,50,50);")
+        :type parent: QWidget
+        :type title: str
+        :type message: str
+        :type buttons: list[QMessageBox.StandardButton]
+        :type headerIcon: str
+        :type headerColor: str
+        :type enableDontShowCheckBox: bool
+        :type force: bool
 
-        p = studioqt.resource.pixmap("critical")
-        mb.setPixmap(p)
+        :rtype: (QMessageBox.StandardButton, bool)
+        """
+        buttons = buttons or (QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.No)
 
-        mb.setWindowTitle(title)
-        mb.setTitleText(title)
+        clickedButton = showMessageBox(
+            parent,
+            title,
+            message,
+            buttons=buttons,
+            headerIcon=headerIcon,
+            headerColor=headerColor,
+            enableDontShowCheckBox=enableDontShowCheckBox,
+            force=force
+        )
 
-        a = mb.exec_()
-        mb.close()
+        return clickedButton
 
-        return mb._standardButtonClicked
+    @staticmethod
+    def critical(
+        parent,
+        title,
+        message,
+        buttons=None,
+        headerIcon="critical",
+        headerColor="rgb(200, 50, 50)"
+    ):
+        """
+        Open a critical message box with the given options.
 
-    def __init__(self, parent=None):
+        :type parent: QWidget
+        :type title: str
+        :type message: str
+        :type headerIcon: str
+        :type headerColor: str
+        :type buttons: list[QMessageBox.StandardButton]
+
+        :rtype: QMessageBox.StandardButton
+        """
+        clickedButton = showMessageBox(
+            parent,
+            title,
+            message,
+            buttons=buttons,
+            headerIcon=headerIcon,
+            headerColor=headerColor
+        )
+
+        return clickedButton
+
+    def __init__(self, parent=None, enableDontShowCheckBox=False):
+
         super(MessageBox, self).__init__(parent)
 
         self.setMinimumWidth(300)
         self.setMaximumWidth(400)
 
-        self._standardButtonClicked = None
+        self._dontShowCheckbox = False
+        self._clickedStandardButton = None
 
         self._header = QtWidgets.QFrame(self)
-        self._header.setStyleSheet("background-color: rgb(50,150,200,0);")
+        self._header.setStyleSheet("background-color: rgb(0,0,0,0);")
         self._header.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         self._header.setFixedHeight(46)
 
@@ -96,8 +273,6 @@ class MessageBox(QtWidgets.QDialog):
         self._message.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
         self._message.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
-        options = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
-
         self._buttonBox = QtWidgets.QDialogButtonBox(None, QtCore.Qt.Horizontal, self)
         self._buttonBox.clicked.connect(self._clicked)
         self._buttonBox.accepted.connect(self.accept)
@@ -105,7 +280,6 @@ class MessageBox(QtWidgets.QDialog):
 
         vlayout1 = QtWidgets.QVBoxLayout(self)
         vlayout1.setContentsMargins(0, 0, 0, 0)
-
         vlayout1.addWidget(self._header)
 
         vlayout2 = QtWidgets.QVBoxLayout(self)
@@ -113,29 +287,100 @@ class MessageBox(QtWidgets.QDialog):
         vlayout2.setContentsMargins(15, 5, 15, 5)
 
         vlayout2.addWidget(self._message)
-        vlayout2.addWidget(self._buttonBox)
 
         vlayout1.addLayout(vlayout2)
+
+        if enableDontShowCheckBox:
+
+            msg = "Don't show this message again"
+            self._dontShowCheckbox = QtWidgets.QCheckBox(msg, self)
+
+            vlayout2.addWidget(self._dontShowCheckbox)
+
+        vlayout2.addWidget(self._buttonBox)
 
         self.setLayout(vlayout1)
 
     def header(self):
+        """
+        Return the header frame.
+
+        :rtype: QtWidgets.QFrame
+        """
         return self._header
 
     def setTitleText(self, text):
+        """
+        Set the title text to be displayed.
+
+        :type text: str
+        :rtype: None 
+        """
         self._title.setText(text)
 
-    def setText(self, message):
-        self._message.setText(message)
+    def setText(self, text):
+        """
+        Set the text message to be displayed.
+        
+        :type text: str
+        :rtype: None 
+        """
+        self._message.setText(text)
 
-    def setOptions(self, options):
-        self._buttonBox.setStandardButtons(options)
+    def setButtons(self, buttons):
+        """
+        Set the buttons to be displayed in message box.
+
+        :type buttons: QMessageBox.StandardButton
+        :rtype: None 
+        """
+        self._buttonBox.setStandardButtons(buttons)
+
+    def setHeaderColor(self, color):
+        """
+        Set the header color for the message box.
+
+        :type color: str
+        :rtype: None 
+        """
+        self.header().setStyleSheet("background-color:" + color)
 
     def setPixmap(self, pixmap):
+        """
+        Set the pixmap for the message box.
+
+        :type pixmap: QWidgets.QPixmap
+        :rtype: None 
+        """
         self._icon.setPixmap(pixmap)
 
     def _clicked(self, button):
-        self._standardButtonClicked = self._buttonBox.standardButton(button)
+        """
+        Triggered when the user clicks a button.
+        
+        :type button: QMessageBox.StandardButton
+        :rtype: None 
+        """
+        self._clickedStandardButton = self._buttonBox.standardButton(button)
+
+    def clickedStandardButton(self):
+        """
+        Return the button that was clicked by the user.
+
+        :rtype: QMessageBox.StandardButton
+        """
+        return self._clickedStandardButton
+
+    def isDontShowCheckboxChecked(self):
+        """
+        Return the checked state of the Dont show again checkbox.
+        
+        :rtype: bool 
+        """
+        if self._dontShowCheckbox:
+            return self._dontShowCheckbox.isChecked()
+        else:
+            return False
 
 
 def showExample():
@@ -144,18 +389,38 @@ def showExample():
 
         title = "Create a snapshot icon"
         message = "Would you like to create a snapshot icon?"
-        options = QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.Ignore | QtWidgets.QDialogButtonBox.Cancel
-        result = MessageBox.question(None, title, message, options)
+        buttons = QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.Ignore | QtWidgets.QDialogButtonBox.Cancel
+        result = MessageBox.question(None, title, message, buttons)
+        print result
 
         title = "Create a snapshot icon"
         message = "This is to test a very long message. This is to test a very long message. This is to test a very long message. This is to test a very long message. This is to test a very long message. "
-        options = QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.Ignore | QtWidgets.QDialogButtonBox.Cancel
-        result = MessageBox.question(None, title, message, options)
+        buttons = QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.Ignore | QtWidgets.QDialogButtonBox.Cancel
+        result = MessageBox.question(None, title, message, buttons)
+        print result
+
+        title = "By Frame Tip"
+        message = "Testing the don't show check box. "
+        buttons = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        result = MessageBox.question(None, title, message, buttons, enableDontShowCheckBox=True)
+        print result
+
+        title = "Create a new thumbnail icon"
+        message = "This will override the existing thumbnail. Are you sure you would like to continue?"
+        buttons = QtWidgets.QDialogButtonBox.Yes | QtWidgets.QDialogButtonBox.No
+        result = MessageBox.warning(None, title, message, buttons, enableDontShowCheckBox=True)
+        print result
+
+        title = "Error saving item!"
+        message = "An error has occurred while saving an item."
+        result = MessageBox.critical(None, title, message)
+        print result
 
         if result == QtWidgets.QDialogButtonBox.Yes:
             title = "Error while saving!"
             message = "There was an error while saving"
-            result = MessageBox.critical(None, title, message)
+            MessageBox.critical(None, title, message)
+
 
 if __name__ == "__main__":
     showExample()
