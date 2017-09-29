55f5ce9e89061be63a76bba71d303eb1efa9872f
krathjen <kurt.rathjen@gmail.com>
2017-09-29 12:48:55 +0100

    Fix root path not being set properly on initial load

--- config.py
+++ config.py
@@ -11,8 +11,5 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-import studiolibrary
-
-
 import studiolibrarymaya
 studiolibrarymaya.setup()
