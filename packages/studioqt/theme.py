1018c2409eb3982a63b9ca9e3eff83eac771e79e
krathjen <kurt.rathjen@gmail.com>
2017-10-12 13:05:50 +0100

    Add an option to change the theme to a custom color Add an example to create more than one instance and change color

--- packages/studioqt/theme.py
+++ packages/studioqt/theme.py
@@ -17,48 +17,60 @@ from studioqt import QtWidgets
 
 import studioqt
 
+
 __all__ = ["Theme", "ThemeAction", "ThemesMenu"]
+
 
 THEME_PRESETS = [
     {
         "name": "Blue",
         "accentColor": "rgb(50, 180, 240, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "backgroundColor": None,
     },
     {
         "name": "Green",
         "accentColor": "rgb(80, 200, 140, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "backgroundColor": None,
     },
     {
         "name": "Yellow",
-        "accentColor": "rgb(250, 200, 0, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(250, 200, 0)",
+        "backgroundColor": None,
     },
     {
         "name": "Orange",
-        "accentColor": "rgb(255, 170, 0, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(255, 170, 0)",
+        "backgroundColor": None,
     },
     {
         "name": "Peach",
-        "accentColor": "rgb(255, 125, 100, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(255, 125, 100)",
+        "backgroundColor": None,
     },
     {
         "name": "Red",
-        "accentColor": "rgb(230, 60, 60, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(230, 60, 60)",
+        "backgroundColor": None,
     },
     {
         "name": "Pink",
-        "accentColor": "rgb(255, 87, 123, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(255, 87, 123)",
+        "backgroundColor": None,
     },
     {
         "name": "Purple",
-        "accentColor": "rgb(110, 110, 240, 255)",
-        "backgroundColor": "rgb(70, 70, 80, 255)",
+        "accentColor": "rgb(110, 110, 240)",
+        "backgroundColor": None,
+    },
+    {
+        "name": "Dark",
+        "accentColor": None,
+        "backgroundColor": "rgb(80, 80, 90)",
+    },
+    {
+        "name": "Light",
+        "accentColor": None,
+        "backgroundColor": "rgb(245, 245, 255)",
     },
 ]
 
@@ -71,9 +83,13 @@ def themePresets():
     """
     themes = []
 
-    for preset in THEME_PRESETS:
+    for data in THEME_PRESETS:
         theme = Theme()
-        theme.setSettings(preset)
+
+        theme.setName(data.get("name"))
+        theme.setAccentColor(data.get("accentColor"))
+        theme.setBackgroundColor(data.get("backgroundColor"))
+
         themes.append(theme)
 
     return themes
@@ -90,12 +106,20 @@ class ThemeAction(QtWidgets.QAction):
         QtWidgets.QAction.__init__(self, theme.name(), *args)
         self._theme = theme
 
-        color = theme.accentColor()
-        icon = studioqt.resource.icon("radio_button_checked_white", color=color)
+        if theme.accentColor():
+            color = theme.accentColor()
+        elif theme.backgroundColor():
+            color = theme.backgroundColor()
+        else:
+            color = "rgb(255,255,255)"
+
+        icon = studioqt.resource.icon(ThemesMenu.THEME_ICON, color=color)
         self.setIcon(icon)
 
     def theme(self):
         """
+        Return the theme object for the action.
+        
         :rtype: Theme
         """
         return self._theme
@@ -103,77 +127,105 @@ class ThemeAction(QtWidgets.QAction):
 
 class ThemesMenu(QtWidgets.QMenu):
 
-    DEFAULT_DARK_COLOR = "rgb(80, 80, 90)"
-    DEFAULT_LIGHT_COLOR = "rgb(230, 230, 235)"
+    THEME_ICON = "radio_button_checked_white"
 
-    DEFAULT_MENU_ICON = "radio_button_checked_white"
+    ENABLE_CUSTOM_ACTION = True
 
-    themeTriggered = QtCore.Signal(object)
-
-    def __init__(self, parent=None, themes=None):
+    def __init__(self, parent, theme, themes=None):
         """
         :type themes: list[Theme]
         :rtype: QtWidgets.QMenu
         """
         QtWidgets.QMenu.__init__(self, "Themes", parent)
 
-        self._currentTheme = None
+        self._theme = theme
+        self._themes = themes
+
+        self.createActions()
+
+    def createActions(self):
+        """
+        Crate the actions to be shown in the menu.
+        
+        :rtype: None 
+        """
+        # Create the menu actions for setting the accent color
+        action = studioqt.SeparatorAction("Accent", self)
+        self.addAction(action)
+
+        themes = self._themes
 
         if not themes:
             themes = themePresets()
 
         for theme in themes:
-            action = ThemeAction(theme, self)
+            if theme.accentColor():
+                action = ThemeAction(theme, self)
+                self.addAction(action)
+
+        if self.ENABLE_CUSTOM_ACTION:
+            action = QtWidgets.QAction("Custom", self)
+            action.triggered.connect(self.theme().browseAccentColor)
+            color = self.theme().accentColor().toString()
+            icon = studioqt.resource.icon(ThemesMenu.THEME_ICON, color=color)
+            action.setIcon(icon)
             self.addAction(action)
 
-        self.addSeparator()
-
-        action = QtWidgets.QAction("Dark", self)
-        icon = studioqt.resource.icon(self.DEFAULT_MENU_ICON, color="rgb(60,60,80)")
-        action.setIcon(icon)
-
+        # Create the menu actions for setting the background color
+        action = studioqt.SeparatorAction("Background", self)
         self.addAction(action)
 
-        action = QtWidgets.QAction("Light", self)
-        icon = studioqt.resource.icon(self.DEFAULT_MENU_ICON, color="rgb(245,245,255)")
-        action.setIcon(icon)
+        for theme in themes:
+            if not theme.accentColor() and theme.backgroundColor():
+                action = ThemeAction(theme, self)
+                self.addAction(action)
 
-        self.addAction(action)
+        if self.ENABLE_CUSTOM_ACTION:
+            action = QtWidgets.QAction("Custom", self)
+            action.triggered.connect(self._theme.browseBackgroundColor)
+            color = self._theme.backgroundColor().toString()
+            icon = studioqt.resource.icon(ThemesMenu.THEME_ICON, color=color)
+            action.setIcon(icon)
+            self.addAction(action)
 
-        self.triggered.connect(self._themeTriggered)
+        self.triggered.connect(self._triggered)
 
-    def setCurrentTheme(self, theme):
-        self._currentTheme = theme
-
-    def currentTheme(self):
+    def theme(self):
         """
-        Triggered when a theme has been clicked.
+        Return the current theme for the menu.
 
-        :rtype: Theme
+        :rtype: studioqt.Theme
         """
-        return self._currentTheme
+        return self._theme
 
-    def _themeTriggered(self, action):
+    def setTheme(self, theme):
+        """
+        Set the current theme for the menu.
+        
+        :type theme: studioqt.Theme
+        :rtype: None 
+        """
+        self._theme = theme
+
+    def _triggered(self, action):
         """
         Triggered when a theme has been clicked.
 
         :type action: Action
         :rtype: None
         """
-        theme = self.currentTheme()
+        theme = self.theme()
         if not theme:
             raise Exception("Please set the current theme for the menu.")
 
-        if isinstance(action, ThemeAction):
+        if not isinstance(action, ThemeAction):
+            return
+
+        if action.theme().accentColor():
             theme.setAccentColor(action.theme().accentColor())
 
-        elif action.text() == "Dark":
-            theme.setBackgroundColor(ThemesMenu.DEFAULT_DARK_COLOR)
-
-        elif action.text() == "Light":
-            theme.setBackgroundColor(ThemesMenu.DEFAULT_LIGHT_COLOR)
-
-        self.themeTriggered.emit(theme)
+        if action.theme().backgroundColor():
+            theme.setBackgroundColor(action.theme().backgroundColor())
 
 
 def showThemesMenu(parent=None, themes=None):
@@ -184,21 +236,22 @@ def showThemesMenu(parent=None, themes=None):
     :type parent: QtWidgets.QWidget
     :rtype: QtWidgets.QAction
     """
-    menu = ThemesMenu(themes=themes, parent=parent)
+    theme = Theme()
+    menu = ThemesMenu(parent=parent, theme=theme, themes=themes)
     position = QtWidgets.QCursor().pos()
     action = menu.exec_(position)
-    return action
+    return theme
 
 
-class Theme(object):
+class Theme(QtCore.QObject):
 
-    LIGHT_THEME_ENABLED = True
-    LIGHT_THEME_THRESHOLD = 210
+    updated = QtCore.Signal()
 
     DEFAULT_ACCENT_COLOR = QtGui.QColor(0, 175, 255)
-    DEFAULT_BACKGROUND_COLOR = QtGui.QColor(70, 70, 80)
+    DEFAULT_BACKGROUND_COLOR = QtGui.QColor(60, 60, 80)
 
     def __init__(self):
+        QtCore.QObject.__init__(self)
 
         self._dpi = 1
 
@@ -237,13 +290,15 @@ class Theme(object):
         name = settings.get("name")
         self.setName(name)
 
-        accentColor = settings.get("accentColor", self.accentColor().toString())
-        accentColor = studioqt.Color.fromString(accentColor)
-        self.setAccentColor(accentColor)
+        color = settings.get("accentColor")
+        if color:
+            color = studioqt.Color.fromString(color)
+            self.setAccentColor(color)
 
-        backgroundColor = settings.get("backgroundColor", self.backgroundColor().toString())
-        backgroundColor = studioqt.Color.fromString(backgroundColor)
-        self.setBackgroundColor(backgroundColor)
+        color = settings.get("backgroundColor")
+        if color:
+            color = studioqt.Color.fromString(color)
+            self.setBackgroundColor(color)
 
     def dpi(self):
         """
@@ -285,15 +340,17 @@ class Theme(object):
 
         rtype: bool
         """
-        if self.LIGHT_THEME_ENABLED:
+        # The luminance for digital formats are (0.299, 0.587, 0.114)
+        red = self.backgroundColor().redF() * 0.299
+        green = self.backgroundColor().greenF() * 0.587
+        blue = self.backgroundColor().blueF() * 0.114
 
-            threshold = self.LIGHT_THEME_THRESHOLD
+        darkness = red + green + blue
 
-            return self.backgroundColor().red() < threshold and \
-                   self.backgroundColor().green() < threshold and \
-                   self.backgroundColor().blue() < threshold
+        if darkness < 0.6:
+            return True
 
-        return True
+        return False
 
     def setDark(self):
         """
@@ -361,7 +418,7 @@ class Theme(object):
         """
         Return the accent color for the theme.
 
-        :rtype: studioqt.Color
+        :rtype: studioqt.Color or None
         """
         return self._accentColor
 
@@ -369,7 +426,7 @@ class Theme(object):
         """
         Return the background color for the theme.
 
-        :rtype: studioqt.Color
+        :rtype: studioqt.Color or None
         """
         return self._backgroundColor
 
@@ -387,6 +444,8 @@ class Theme(object):
 
         self._accentColor = color
 
+        self.updated.emit()
+
     def setBackgroundColor(self, color):
         """
         Set the background color for the theme.
@@ -401,47 +460,110 @@ class Theme(object):
 
         self._backgroundColor = color
 
+        self.updated.emit()
+
+    def createColorDialog(
+            self,
+            parent,
+            standardColors=None,
+            currentColor=None,
+    ):
+        """
+        Create a new instance of the color dialog.
+
+        :type parent: QtWidgets.QWidget
+        :type standardColors: list[int]
+        :rtype: QtWidgets.QColorDialog
+        """
+        dialog = QtWidgets.QColorDialog(parent)
+
+        if standardColors:
+            index = -1
+            for colorR, colorG, colorB in standardColors:
+                index += 1
+
+                color = QtGui.QColor(colorR, colorG, colorB).rgba()
+
+                try:
+                    # Support for new qt5 signature
+                    color = QtGui.QColor(color)
+                    dialog.setStandardColor(index, color)
+                except:
+                    # Support for new qt4 signature
+                    color = QtGui.QColor(color).rgba()
+                    dialog.setStandardColor(index, color)
+
+        # PySide2 doesn't support d.open(), so we need to pass a blank slot.
+        dialog.open(self, QtCore.SLOT("blankSlot()"))
+
+        if currentColor:
+            dialog.setCurrentColor(currentColor)
+
+        return dialog
+
     def browseAccentColor(self, parent=None):
         """
-        Show the color dialog to browser for a accent color.
-
+        Show the color dialog for changing the accent color.
+        
         :type parent: QtWidgets.QWidget
         :rtype: None
         """
-        color = self.accentColor()
-        dialog = QtWidgets.QColorDialog(parent)
-        dialog.setCurrentColor(color)
-        dialog.connect(
-            dialog,
-            QtCore.SIGNAL("setAccentColor(const QColor&)"),
-            self.setAccentColor
-        )
-        dialog.open()
+        standardColors = [
+            (230, 60, 60), (210, 40, 40), (190, 20, 20), (250, 80, 130),
+            (230, 60, 110), (210, 40, 90), (255, 90, 40), (235, 70, 20),
+            (215, 50, 0), (240, 100, 170), (220, 80, 150), (200, 60, 130),
+            (255, 125, 100), (235, 105, 80), (215, 85, 60), (240, 200, 150),
+            (220, 180, 130), (200, 160, 110), (250, 200, 0), (230, 180, 0),
+            (210, 160, 0), (225, 200, 40), (205, 180, 20), (185, 160, 0),
+            (80, 200, 140), (60, 180, 120), (40, 160, 100), (80, 225, 120),
+            (60, 205, 100), (40, 185, 80), (50, 180, 240), (30, 160, 220),
+            (10, 140, 200), (100, 200, 245), (80, 180, 225), (60, 160, 205),
+            (130, 110, 240), (110, 90, 220), (90, 70, 200), (180, 160, 255),
+            (160, 140, 235), (140, 120, 215), (180, 110, 240), (160, 90, 220),
+            (140, 70, 200), (210, 110, 255), (190, 90, 235), (170, 70, 215)
+        ]
+
+        currentColor = self.accentColor()
+
+        dialog = self.createColorDialog(parent, standardColors, currentColor)
+        dialog.currentColorChanged.connect(self.setAccentColor)
+
         if dialog.exec_():
             self.setAccentColor(dialog.selectedColor())
         else:
-            self.setAccentColor(color)
+            self.setAccentColor(currentColor)
 
     def browseBackgroundColor(self, parent=None):
         """
-        Show the color dialog to browser for a background color.
+        Show the color dialog for changing the background color.
 
         :type parent: QtWidgets.QWidget
         :rtype: None
         """
-        color = self.backgroundColor()
-        dialog = QtWidgets.QColorDialog(parent)
-        dialog.setCurrentColor(color)
-        dialog.connect(
-            dialog,
-            QtCore.SIGNAL("setBackgroundColor(const QColor&)"),
-            self.setBackgroundColor
-        )
-        dialog.open()
+        standardColors = [
+            (0, 0, 0), (20, 20, 20), (40, 40, 40), (60, 60, 60),
+            (80, 80, 80), (100, 100, 100), (20, 20, 30), (40, 40, 50),
+            (60, 60, 70), (80, 80, 90), (100, 100, 110), (120, 120, 130),
+            (0, 30, 60), (20, 50, 80), (40, 70, 100), (60, 90, 120),
+            (80, 110, 140), (100, 130, 160), (0, 60, 60), (20, 80, 80),
+            (40, 100, 100), (60, 120, 120), (80, 140, 140), (100, 160, 160),
+            (0, 60, 30), (20, 80, 50), (40, 100, 70), (60, 120, 90),
+            (80, 140, 110), (100, 160, 130), (60, 0, 10), (80, 20, 30),
+            (100, 40, 50), (120, 60, 70), (140, 80, 90), (160, 100, 110),
+            (60, 0, 40), (80, 20, 60), (100, 40, 80), (120, 60, 100),
+            (140, 80, 120), (160, 100, 140), (40, 15, 5), (60, 35, 25),
+            (80, 55, 45), (100, 75, 65), (120, 95, 85), (140, 115, 105)
+        ]
+
+        currentColor = self.backgroundColor()
+
+        dialog = self.createColorDialog(parent, standardColors, currentColor)
+        dialog.currentColorChanged.connect(self.setBackgroundColor)
+
         if dialog.exec_():
             self.setBackgroundColor(dialog.selectedColor())
         else:
-            self.setBackgroundColor(color)
+            self.setBackgroundColor(currentColor)
 
     def options(self):
         """
@@ -514,9 +636,9 @@ def example():
 
     :rtype: None
     """
-    themeAction = showThemesMenu()
-    print "Accent color:", themeAction.theme().accentColor()
-    print "Background color:", themeAction.theme().backgroundColor()
+    theme = showThemesMenu()
+    print "Accent color:", theme.accentColor()
+    print "Background color:", theme.backgroundColor()
 
 
 if __name__ == "__main__":
