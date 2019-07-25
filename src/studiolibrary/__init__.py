0d3f204743f17f7058eb398bf85076f097815ec3
krathjen <kurt.rathjen@gmail.com>
2019-07-25 22:44:03 +1000

    Version 2.5.4

--- src/studiolibrary/__init__.py
+++ src/studiolibrary/__init__.py
@@ -10,7 +10,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.5.3"
+__version__ = "2.5.4"
 
 
 def version():
