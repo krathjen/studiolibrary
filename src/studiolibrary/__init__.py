61576e1e5da3e5955309c5859eab0324a8e0e827
krathjen <kurt.rathjen@gmail.com>
2019-08-12 21:47:54 +1000

    Version 2.6.0.b6

--- src/studiolibrary/__init__.py
+++ src/studiolibrary/__init__.py
@@ -10,7 +10,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.6.0.b5"
+__version__ = "2.6.0.b6"
 
 
 def version():
