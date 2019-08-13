240296a967f382297706bd774c4618e5f949201a
krathjen <kurt.rathjen@gmail.com>
2019-08-13 22:11:41 +1000

    Version 2.6.0.b8

--- src/studiolibrary/__init__.py
+++ src/studiolibrary/__init__.py
@@ -10,7 +10,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.6.0.b7"
+__version__ = "2.6.0.b8"
 
 
 def version():
