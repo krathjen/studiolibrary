20c24372c041c52798daba79b7533f5587347a47
krathjen <kurt.rathjen@gmail.com>
2019-09-24 19:52:18 +1000

    Version 2.7.0.b5

--- src/studiolibrary/__init__.py
+++ src/studiolibrary/__init__.py
@@ -10,7 +10,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.7.0.b4"
+__version__ = "2.7.0.b5"
 
 
 def version():
