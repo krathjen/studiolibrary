bedf9cd9bc94a8789dc816abd6f1e861af9b7d40
Claudia Fujiwara <claudia.fujiwara@reelfx.com>
2020-06-18 09:26:31 -0400

    Add root namespace (':') to the return of getAll to fix  ValueError when applying a pose to a character without namespace

--- src/mutils/namespace.py
+++ src/mutils/namespace.py
@@ -1,11 +1,11 @@
 # Copyright 2019 by Kurt Rathjen. All Rights Reserved.
 #
-# This library is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU Lesser General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or 
-# (at your option) any later version. This library is distributed in the 
-# hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# This library is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. This library is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library. If not, see <http://www.gnu.org/licenses/>.
@@ -74,8 +74,8 @@ def getFromSelection():
 def getAll():
     """
     Get all the available namespaces in the scene
-    
-    :rtype: list[str] 
+
+    :rtype: list[str]
     """
     IGNORE_NAMESPACES = ['UI', 'shared']
 
@@ -83,4 +83,4 @@ def getAll():
     namespaces = list(set(namespaces) - set(IGNORE_NAMESPACES))
     namespaces = sorted(namespaces)
 
-    return namespaces
+    return namespaces + [':']
